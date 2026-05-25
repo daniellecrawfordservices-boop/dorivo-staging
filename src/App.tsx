6c9bcd0590e7305e7f842e383666d4744211b559
daniellecrawfordservices-boop <daniellecrawfordservices@gmail.com>
2026-05-25 14:54:28 +0300

    feat(content): wire /articles/anti-aging-skincare-tips route

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsConditions from "./pages/TermsConditions";
 import CryptoMining from "./pages/articles/CryptoMining";
+import AntiAgingSkincareTips from "./pages/articles/AntiAgingSkincareTips";
 
 const queryClient = new QueryClient();
 
@@ -50,6 +51,8 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
                 {/* DOMAINER_ROUTE:CryptoMining */}
         <Route path="/articles/crypto-mining" element={<CryptoMining />} />
+        {/* DOMAINER_ROUTE:AntiAgingSkincareTips */}
+        <Route path="/articles/anti-aging-skincare-tips" element={<AntiAgingSkincareTips />} />
 </Routes>
       </BrowserRouter>
     </TooltipProvider>
